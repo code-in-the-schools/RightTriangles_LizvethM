5f538421b4e5c6d8f923e3f240cca7e7c58c753c
LizvethM <morales-garciaLizveth2021@email.com>
2020-07-24 18:01:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#UserInput
+char= input("Enter a character: ")
+num = int(input("Enter a numer: "))
+#triangle 1text
+for i in range(1, num+1):
+  text=""
+  for j in range (i):
+    text+= char
+
+  print(text)
+
+print()
+
+for i in range (len(char)):
+  for j in range (int(num)):
+    print(char*num)
+    num= num- 1
+
+  print()
+
+#for i in range(0,num):
+  for j in range(0,char):
+    print(
